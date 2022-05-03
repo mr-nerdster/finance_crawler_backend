7e5661d4a1963042ad50cbb84a5c0140a97b7ad0
NightmareNight-em <nigamharshit1712@gmail.com>
2022-05-03 19:59:38 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const cors = require("cors");
 //setting up cors
 app.use(cors());
 
-const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
+// const Sentry = require("@sentry/node");
+// const Tracing = require("@sentry/tracing");
 const express = require("express");
 const dotenv = require("dotenv");
 const db = require("./config/dbConnect");
@@ -24,23 +24,23 @@ app.use(express.json()); // to allow posting json data
 
 // ------------------ Sentry ----------------------
 
-Sentry.init({
-  dsn: "https://b0e166ee11434bbe9f544e8a35576bc8@o1230295.ingest.sentry.io/6376883",
+// Sentry.init({
+//   dsn: "https://b0e166ee11434bbe9f544e8a35576bc8@o1230295.ingest.sentry.io/6376883",
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Tracing.Integrations.Express({
-          app,
-      }),
-  ],
-  tracesSampleRate: 1.0,
-});
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   integrations: [
+//       new Sentry.Integrations.Http({ tracing: true }),
+//       new Tracing.Integrations.Express({
+//           app,
+//       }),
+//   ],
+//   tracesSampleRate: 1.0,
+// });
 
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
+// app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.tracingHandler());
 // -------------------------------------------------
 
 
